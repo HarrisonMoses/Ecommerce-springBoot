6f1b5bfeb89cd7f5cbfec1fcad53eef0339bfc8b
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-07-25 10:31:49 +0300

    Added sorting to users by email and name

--- store/src/main/java/com/harrisonmoses/store/controllers/UserController.java
+++ store/src/main/java/com/harrisonmoses/store/controllers/UserController.java
@@ -5,14 +5,12 @@ import com.harrisonmoses.store.Mappers.UserMapper;
 import com.harrisonmoses.store.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.harrisonmoses.store.Entity.User;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -23,8 +21,15 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping("")
-    public Iterable<UserDto> getUsers() {
-       return userRepository.findAll().stream()
+    public Iterable<UserDto> getUsers(
+           @RequestParam( required= false, defaultValue = "", name ="sort") String sort
+    ) {
+        if(!Set.of("name", "email").contains(sort)){
+            sort = "name";
+        }
+
+       return userRepository.findAll(Sort.by(sort))
+               .stream()
                .map(userMapper::toDto)
                .toList();
     }
