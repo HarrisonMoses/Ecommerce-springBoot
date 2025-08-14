844c6ad1093e5378e97519097686ae13a5eabc9f
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-08-14 11:58:46 +0300

    modified Authcontroller Exception handling

--- store/src/main/java/com/harrisonmoses/store/controllers/AuthController.java
+++ store/src/main/java/com/harrisonmoses/store/controllers/AuthController.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -86,9 +87,8 @@ public class AuthController {
     }
 
 
-//    @ExceptionHandler(BadCredentialsException.class)
-//    public ResponseEntity<Void> unAuthorized(){
-//
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> unAuthorized(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
